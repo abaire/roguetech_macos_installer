44713364ce3cac1b9cb8b84a3af2aed5144f5656
Erik Abair <erik.abair@gmail.com>
2022-08-24 08:24:19 -0700

    Adds option to suppress git updates.

--- roguetech_install_macos.sh
+++ roguetech_install_macos.sh
@@ -3,6 +3,7 @@
 
 set -eu
 set -o pipefail
+set -x
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 readonly STEAM_INSTALL_DIR="${HOME}/Library/Application Support/Steam/steamapps/common/BATTLETECH/BattleTech.app"
@@ -20,6 +21,36 @@ if [[ ! $(which git) ]]; then
   exit 1
 fi
 
+NOUPDATE=false
+
+while [[ $# -gt 0 ]] && [[ "$1" == "--"* ]]; do
+    opt="$1"
+    shift
+
+    case "$opt" in
+      "--noupdate" )
+        NOUPDATE=true
+        ;;
+      "--help" )
+        echo >&2 "Options:"
+        echo >&2 "  --noupdate : Suppress checking for updates of RogueTech data."
+        exit 1
+        ;;
+      *)
+        echo >&2 "Invalid option: $@"
+        exit 1
+        ;;
+   esac
+done
+
+
+function SymlinkIfNeeded() {
+  local source="${1}"
+  local link="${2}"
+  if [[ ! -L "${link}" ]]; then
+    ln -s "${source}" "${link}"
+  fi
+}
 
 function GitClone() {
   local target_dir="${1}"
@@ -27,11 +58,11 @@ function GitClone() {
 
   if [[ ! -d "${1}" ]]; then
     git clone --depth 1 "${2}" "${1}"
-  else
-    pushd "${1}"
+  elif [[ ! ${NOUPDATE} ]]; then
+    pushd "${1}" >/dev/null
     git checkout -- .
     git fetch origin --depth 1
-    popd
+    popd >/dev/null
   fi
 }
 
@@ -44,62 +75,56 @@ MOD_DIR="${RESOURCES}/Mods"
 BATTLETECH_DATA_DIR="${CONTENTS}/MacOS/BattleTech__Data"
 
 mkdir -p "${MOD_DIR}"
-if [[ ! -L "${BATTLETECH_DATA_DIR}" ]]; then
-  ln -s "${RESOURCES}/Data" "${BATTLETECH_DATA_DIR}"
-fi
-if [[ ! -L "${BASE}/../Mods" ]]; then
-  ln -s "${MOD_DIR}" "${BASE}/../Mods"
-fi
-if [[ ! -L "${CONTENTS}/MacOS/Mods" ]]; then
-  ln -s "${MOD_DIR}" "${CONTENTS}/MacOS/Mods"
-fi
+SymlinkIfNeeded "${RESOURCES}/Data" "${BATTLETECH_DATA_DIR}"
+SymlinkIfNeeded "${MOD_DIR}" "${BASE}/../Mods"
+SymlinkIfNeeded "${MOD_DIR}" "${CONTENTS}/MacOS/Mods"
 
-pushd "${BASE}/.."
+pushd "${BASE}/.." >/dev/null
 mkdir -p RtlCache
-pushd RtlCache
+pushd RtlCache >/dev/null
 
 GitClone RtCache https://github.com/BattletechModders/RogueTech.git
 GitClone RogueData https://github.com/wmtorode/RogueLauncherData.git
 
 mkdir -p CabCache
-pushd CabCache
+pushd CabCache >/dev/null
 GitClone cabClan https://github.com/BattletechModders/Community-Asset-Bundle-Clan-Mech.git
 GitClone cabIs https://github.com/BattletechModders/Community-Asset-Bundle-IS-Mech.git
 GitClone cabMisc https://github.com/BattletechModders/Community-Asset-Bundle-Miscellaneous.git
 GitClone CabSupRepoData https://github.com/BattletechModders/Community-Asset-Bundle-Data.git
 GitClone cabTank https://github.com/BattletechModders/Community-Asset-Bundle-Tanks.git
-popd
+popd >/dev/null  # CabCache
 
-popd
+popd >/dev/null  # RtlCache
 
 RTLCACHE="${BASE}/../RtlCache"
 
 # Symlink the full install repos into the mod directory.
-pushd "${MOD_DIR}"
+pushd "${MOD_DIR}" >/dev/null
 mkdir -p RtlCache
 
-pushd RtlCache
-ln -s "${RTLCACHE}/RogueData" RogueData
-ln -s "${RTLCACHE}/CabCache" CabCache
-popd RtlCache
-
-# pop RtlCache
-popd
+pushd RtlCache >/dev/null
+SymlinkIfNeeded "${RTLCACHE}/RogueData" RogueData
+SymlinkIfNeeded "${RTLCACHE}/CabCache" CabCache
+popd RtlCache >/dev/null
 
 # pop MOD_DIR
+popd >/dev/null
+
+# pop BASE/..
 popd
 
 
 
 function install_rt_subcomponents() {
   local RTCACHE="${BASE}/../RtlCache/RtCache"
-  pushd "${MOD_DIR}"
-  
+  pushd "${MOD_DIR}" >/dev/null
+
   if [[ ! -d ModTek ]]; then
     cp -R "${RTCACHE}/ModTek" .
   fi
 
-  pushd ModTek
+  pushd ModTek >/dev/null
   mono ModTekInjector.exe /restore /manageddir="${BATTLETECH_DATA_DIR}/Managed/"
   mono ModTekInjector.exe /install /y /manageddir="${BATTLETECH_DATA_DIR}/Managed/"
   popd
@@ -131,7 +156,7 @@ function install_rt_subcomponents() {
   done
 
   # TODO: Handle the optional installs rom RtConfig.xml.
-  
+
   popd # MOD_DIR
 }
 install_rt_subcomponents
