754222ac039a1a90c2e2530c05ded71dc2af67da
Erik Abair <erik.abair@gmail.com>
2022-09-25 14:36:58 -0700

    Adds basic configuration GUI.

--- README.md
+++ README.md
@@ -9,10 +9,13 @@ Script to install [RogueTech](https://roguetech.fandom.com/wiki/Installation#mac
 * `json5` Python module
 * `xmltodict` Python module
 
-The script should check for missing requirements and give hints for how to install them.
+## Optional for GUI
+* `rich` Python module
+* `sshkeyboard` Python module
+
+The script checks for missing requirements and gives hints for how to install them.
 
 # Limitations
-* The script just uses the default RogueTech selections from [the config file](https://github.com/BattletechModders/RogueTech/blob/master/RtConfig.xml). In theory you could modify the copy of the file in the `RtlCache/RtCache` directory created next to your `BattleTech.app` binary and change `<isSelected>false</isSelected>` to `<isSelected>true</isSelected>` for items you'd like to enable and then rerun the installer script, but this is not tested.
 * The script does not support all install task methods, so some options may not be available even if they are manually enabled.
 * The script ignores some of the install task attributes that may actually be needed. I have not run the actual RogueTechLauncher and do not know what things like `difficultyModifier` and `influenceMode` are meant to do, so if they're anything more than informational displays in the launcher itself, they're not wired up right now.
 * The script does not provide any mechanism to uninstall. Outside of running ModTek `/restore` and deleting the Mod directory I'm not sure what else would need to be cleaned up.
--- roguetech_install_macos.py
+++ roguetech_install_macos.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import collections
 from collections import defaultdict
 import glob
 import json
@@ -25,6 +26,22 @@ except ImportError:
     print("Try 'pip3 install xmltodict'")
     sys.exit(1)
 
+try:
+    import sshkeyboard
+    import rich
+    from rich import console
+    from rich.align import Align
+    from rich.columns import Columns
+    from rich.layout import Layout
+    from rich.prompt import Prompt
+    from rich.rule import Rule
+    from rich.style import Style
+    from rich.table import Table
+    from rich.text import Text
+
+    _GUI_ENABLED = True
+except ImportError:
+    _GUI_ENABLED = False
 
 _STEAM_INSTALL_DIR = (
     "~/Library/Application Support/Steam/steamapps/common/BATTLETECH/BattleTech.app"
@@ -35,7 +52,7 @@ _RT_CONFIG_FILENAME = "RtConfig.xml"
 _TASK_BLACKLIST = {
     "modtekInstall",  # Handled by this install script directly.
     "perfixInstall",  # Appears to cause the game to become stuck when prewarming cache or loading blocks.
-    "CommanderPortraitLoader", # Included in the Core by default.
+    "CommanderPortraitLoader",  # Included in the Core by default.
 }
 
 
@@ -102,12 +119,21 @@ def _load_xml_dict(path: str) -> dict:
 
 
 def _index_tasks_by_option_group(config: dict) -> dict:
-    """Returns a dict mappibng optionGroupId -> [InstallTask]"""
+    """Returns a dict mapping optionGroupId -> [InstallTask]"""
     ret = defaultdict(list)
     tasks = config["RogueTechConfig"]["Tasks"]["InstallTask"]
     for task in tasks:
         group = task["optionGroupId"]
         ret[group].append(task)
+    return ret
+
+
+def _index_tasks_by_id(config: dict) -> dict:
+    """Returns a dict mapping Id -> [InstallTask]"""
+    tasks = config["RogueTechConfig"]["Tasks"]["InstallTask"]
+    ret = {}
+    for task in tasks:
+        ret[task["Id"]] = task
     return ret
 
 
@@ -145,7 +171,7 @@ def _filtered_copy(source_path: str, target_path: str, excludes: Set[str]):
 class Installer:
     """RogueTech installer."""
 
-    def __init__(self, bt_install_path: str, check_updates=True):
+    def __init__(self, bt_install_path: str, check_updates=True, dry_run=False):
         self.base = bt_install_path
         self.contents = os.path.join(self.base, "Contents")
         self.resources = os.path.join(self.contents, "Resources")
@@ -159,6 +185,7 @@ class Installer:
         self.cabcache = os.path.join(self.rtlcache, "CabCache")
 
         self.check_updates = check_updates
+        self.dry_run = dry_run
 
     def git(self, target_path: str, repo_url: str):
         """Fetches or updates a git repository."""
@@ -375,8 +402,24 @@ class Installer:
             f"Skipping unsupported install type {jobtype} for task {task_id}"
         )
 
-    def perform_install(self):
+    def _load_config(self, preserve_existing=True):
+        if preserve_existing:
+            try:
+                config = _load_xml_dict(self._installed_config_path())
+                logging.debug("Using previously installed config file.")
+                return config
+            except FileNotFoundError:
+                return self._load_config(False)
+
+        config = _load_xml_dict(self.rtconfig)
+        logging.debug("Using default config file.")
+        return config
+
+    def perform_install(self, config=None):
         """Installs RogueTech."""
+        if self.dry_run:
+            logging.info("Dry-run mode, bypassing install")
+            return
 
         logging.info("Installing ModTek...")
         injector = os.path.join(self.mod_dir, "ModTek")
@@ -385,11 +428,16 @@ class Installer:
 
         self._install_injector(injector)
 
-        logging.info("Copying RogueTech config...")
-        shutil.copy2(self.rtconfig, self._installed_config_path())
+        if not config:
+            config = self._load_config()
+            logging.info("Copying RogueTech config...")
+            shutil.copy2(self.rtconfig, self._installed_config_path())
+        else:
+            logging.info("Writing updated RogueTech config...")
+            with open(self._installed_config_path(), "w", encoding="utf-8") as outfile:
+                 outfile.write(xmltodict.unparse(config, pretty=True))
 
         logging.info("Performing install...")
-        config = _load_xml_dict(self.rtconfig)
         tasks = _get_selected_tasks(config)
         for task in tasks:
             logging.debug(f"InstallTask {task}")
@@ -417,23 +465,52 @@ class Installer:
                 )
             )
 
-    def list_install_configuration(self):
-        """Prints the current install configuration."""
-
-        try:
-            config = _load_xml_dict(self._installed_config_path())
-            logging.debug("Using previously installed config file.")
-        except FileNotFoundError:
-            config = _load_xml_dict(self.rtconfig)
-            logging.debug("Using default config file.")
-
+    def is_option_enabled(self, config, option_id):
+        tasks = config["RogueTechConfig"]["Tasks"]["InstallTask"]
         task_index = _index_tasks_by_option_group(config)
+        for task in tasks:
+            if task["Id"] != option_id:
+                continue
+            return task["isSelected"].lower() == "true"
+        return False
 
+    def set_option(self, config, group_id: str, task_id: str, new_value: bool):
+        groups, task_group_index = self._extract_configuration_tree(config)
+        task_index = _index_tasks_by_id(config)
+        group = groups[group_id]
+        option_type = group["optionType"]
+        if option_type == "ExclusiveOption":
+            if not new_value:
+                raise ValueError("Attempt to disable an exclusive option.")
+
+            task_group = task_group_index[group_id]
+            for task in task_group:
+                task["isSelected"] = "true" if task["Id"] == task_id else "false"
+
+        elif option_type == "MultiSelectOption":
+            task_index[task_id]["isSelected"] = "true" if new_value else "false"
+        else:
+            raise ValueError(f"Unsupported option type {option_type}")
+
+    @staticmethod
+    def _extract_configuration_tree(config):
+        task_index = _index_tasks_by_option_group(config)
         groups = {}
         options = config["RogueTechConfig"]["Options"]["InstallOption"]
         for option in options:
             group_id = option["optionId"]
             groups[group_id] = option
+        return groups, task_index
+
+    def get_configuration_tree(self, preserve_existing=True):
+        """Parses the configuration file into groups and a task index"""
+        config = self._load_config(preserve_existing)
+        groups, task_index = self._extract_configuration_tree(config)
+        return config, groups, task_index
+
+    def list_install_configuration(self):
+        """Prints the current install configuration."""
+        _, groups, task_index = self.get_configuration_tree()
 
         for group_id, group in groups.items():
             tasks = filter(lambda x: x["canSelect"] == "true", task_index[group_id])
@@ -448,6 +525,195 @@ class Installer:
                 else:
                     self._print_task_info(task, "- ")
             print("")
+
+
+def clamp(val: int, min_val: int, max_val: int) -> int:
+    if val < min_val:
+        return min_val
+    if val > max_val:
+        return max_val
+    return val
+
+
+class _App:
+    _MENU = "menu"
+    _CONTENT = "content"
+
+    def __init__(self, installer: Installer):
+        self._installer = installer
+
+        self._current_group_index: int = 0
+        self._cursor_row = 0
+        self.reset()
+        self._console = console.Console()
+        self._root = Layout()
+        self._running = False
+        self._update()
+        self._keymap = {
+            "left": lambda: self.navigate_group(-1),
+            "right": lambda: self.navigate_group(1),
+            "up": lambda: self.navigate_task(-1),
+            "down": lambda: self.navigate_task(1),
+            "pageup": lambda: self.navigate_task(-5),
+            "pagedown": lambda: self.navigate_task(5),
+            "home": lambda: self.navigate_task(-1000000000),
+            "end": lambda: self.navigate_task(1000000000),
+            "space": self.toggle_task,
+            "enter": self.toggle_task,
+            "d": lambda: self.reset(False),
+            "r": self.reset,
+            "i": self.install,
+        }
+
+    def reset(self, preserve_existing=True):
+        self._config, groups, self._task_index = self._installer.get_configuration_tree(preserve_existing)
+        self._groups = []
+        for group_id, group in groups.items():
+            tasks = list(filter(lambda x: x["canSelect"] == "true", self._task_index[group_id]))
+            if not tasks:
+                continue
+            self._groups.append((group, tasks))
+
+    def install(self):
+        self._installer.perform_install(self._config)
+
+    def navigate_group(self, delta: int):
+        new_index = clamp(
+            self._current_group_index + delta, 0, len(self._groups) - 1
+        )
+        if new_index == self._current_group_index:
+            return
+        self._current_group_index = new_index
+        self._cursor_row = 0
+
+    def navigate_task(self, delta: int):
+        _, tasks = self._groups[self._current_group_index]
+        new_index = clamp(
+            self._cursor_row + delta, 0, len(tasks) - 1
+        )
+        if new_index == self._cursor_row:
+            return
+        self._cursor_row = new_index
+
+    def toggle_task(self):
+        group, tasks = self._groups[self._current_group_index]
+        task = tasks[self._cursor_row]
+        option_type = group["optionType"]
+        required_option = group.get("requiredOption")
+        if required_option and not self._installer.is_option_enabled(self._config, required_option):
+            return
+
+        current_val = task["isSelected"].lower() == "true"
+
+        if option_type == "ExclusiveOption":
+            if current_val:
+                # Don't allow exclusive options to be completely deselected.
+                return
+            current_val = True
+        elif option_type == "MultiSelectOption":
+            current_val = not current_val
+        else:
+            raise ValueError(f"Unsupported option type {option_type}")
+        self._installer.set_option(self._config, group["optionId"], task["Id"], current_val)
+        self._update()
+
+    def _update(self):
+        group, tasks = self._groups[self._current_group_index]
+
+        required_option = group.get("requiredOption")
+        disabled = False
+        if required_option:
+            disabled = not self._installer.is_option_enabled(self._config, required_option)
+            # TODO: Handle OnHideSelect field when an option is disabled.
+        option_type = group["optionType"]
+
+        title = group["optionUiName"]
+        description = group.get("optionDescription")
+
+        if disabled:
+            title_line = Text(f"{title} <DISABLED, requires {required_option}>", style=Style(dim=True, strike=True))
+        else:
+            title_line = Text(title)
+        cols = [Layout(Align(title_line, align="center"), ratio=0)]
+        if description and description != title:
+            cols.append(Layout(Text(description), ratio=0))
+
+        if disabled:
+            cols.append(Align(Text(f"This content is disabled because it depends on the option {required_option} which is not enabled.", style=Style(bold=True, italic=True)), align="center", vertical="middle"))
+        else:
+            table = Table(show_lines=True, expand=True)
+            table.add_column("", no_wrap=True)
+            table.add_column("Option")
+            table.add_column("Description")
+            table.add_column("Diff")
+            table.add_column("Save breaking?")
+
+            for i, task in enumerate(tasks):
+                if option_type == "MultiSelectOption":
+                    prefix = "X" if task["isSelected"] == "true" else "_"
+                else:
+                    prefix = "+" if task["isSelected"] == "true" else "-"
+                if self._cursor_row == i and not disabled:
+                    prefix = Text.styled(f"[{prefix}]", style=Style(bold=True, blink=True))
+                else:
+                    prefix = f" {prefix} "
+                save_breaking = "YES" if task["saveBreaking"] == "true" else " "
+
+                table.add_row(prefix, task["uiName"], task["uiDescription"], task["difficultyModifier"], save_breaking)
+
+            cols.append(table)
+
+        cols.append(Layout(Align(Text(f"{self._current_group_index + 1}/{len(self._groups)}"), align="right"), ratio=0))
+        cols.append(
+            Layout(Align(Text("ESC: exit, SPACEBAR: toggle, i: install, d: reset options to default, r: reset options to installed, cursor keys: navigate"), align="center"),
+                   ratio=0))
+
+        self._root.split_column(*cols)
+
+    def render(self):
+        """Draws the application to the console."""
+        rich.print(self._root)
+
+    def _handle_key(self, key):
+        if key == "esc" or key == "q":
+            self._running = False
+            return
+
+        action = self._keymap.get(key, None)
+        if action:
+            action()
+            return
+
+        logging.info(f"Unhandled key {key}")
+
+    def run(self):
+
+        input_queue = []
+
+        def handle_key(key):
+            input_queue.append(key)
+            sshkeyboard.stop_listening()
+
+        self._running = True
+        with self._console.screen() as screen:
+            screen.update(self._root)
+
+            try:
+                while self._running:
+                    input_queue.clear()
+                    sshkeyboard.listen_keyboard(
+                        on_press=handle_key,
+                        until=None,
+                        sequential=True,
+                        delay_second_char=10,
+                    )
+                    for key in input_queue:
+                        self._handle_key(key)
+                    if self._running:
+                        self._update()
+                        screen.update(self._root)
+            except KeyboardInterrupt:
+                return
 
 
 def _main(args):
@@ -471,8 +737,14 @@ def _main(args):
         )
         return 1
 
-    installer = Installer(bt_install_dir, not args.noupdate)
+    if args.dry_run:
+        args.noupdate = True
+    installer = Installer(bt_install_dir, not args.noupdate, args.dry_run)
     installer.cache_roguetech_files()
+
+    if _GUI_ENABLED and not args.no_gui:
+        app = _App(installer)
+        return app.run()
 
     if args.list:
         installer.list_install_configuration()
@@ -484,6 +756,7 @@ def _main(args):
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
+
 
     def _parse_args():
         parser = argparse.ArgumentParser()
@@ -503,8 +776,21 @@ if __name__ == "__main__":
         )
 
         parser.add_argument(
+            "--dry-run",
+            action="store_true",
+            help="Do not actually modify any content.",
+        )
+
+        parser.add_argument(
+            "--no-gui",
+            action="store_true",
+            help="Suppress the graphical user interface.",
+        )
+
+        parser.add_argument(
             "-v", "--verbose", action="store_true", help="Print verbose logging output."
         )
         return parser.parse_args()
+
 
     sys.exit(_main(_parse_args()))
